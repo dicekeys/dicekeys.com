cb6ce4c849bf591b33b1a9e7a5fdfa4e5fb650d6
Stuart Schechter <stuart@dicekeys.com>
2021-02-26 10:18:52 +0900

    stan's v2

--- js/main.js
+++ js/main.js
@@ -1,71 +1,124 @@
-var presentationItem = document.getElementsByClassName('header__presentation-instruction-item');
-var presentation = document.getElementsByClassName('presentation');
-var presentationTitle = document.getElementsByClassName('header__presentation-title');
 
-presentationItem[0].onclick = function() {
-    presentationItem[1].classList.remove("active");
-    presentationItem[2].classList.remove("active");
-    presentationItem[3].classList.remove("active");
-    (this).classList.add("active");
+document.addEventListener('DOMContentLoaded', () => {
 
-    presentation[1].classList.remove("active");
-    presentation[2].classList.remove("active");
-    presentation[3].classList.remove("active");
-    presentation[0].classList.add("active");
+    if (document.querySelector('#home')) { // what`s page
+        const presentationItem = document.querySelectorAll('.header__presentation-instruction-item');
+        const presentation = document.querySelectorAll('.presentation');
+        const presentationTitle = document.querySelector('.header__presentation-title');
 
-    presentationTitle[1].classList.remove("active");
-    presentationTitle[0].classList.add("active");
-}
-
-presentationItem[1].onclick = function() {
-
-    presentationItem[0].classList.remove("active");
-    presentationItem[2].classList.remove("active");
-    presentationItem[3].classList.remove("active");
-    (this).classList.add("active");
-
-    presentation[0].classList.remove("active");
-    presentation[2].classList.remove("active");
-    presentation[3].classList.remove("active");
-    presentation[1].classList.add("active");
-
-    presentationTitle[1].classList.remove("active");
-    presentationTitle[0].classList.add("active");
-}
-
-presentationItem[2].onclick = function() {
-      presentationItem[0].classList.remove("active");
-      presentationItem[1].classList.remove("active");
-      presentationItem[3].classList.remove("active");
-      (this).classList.add("active");
-
-      presentation[0].classList.remove("active");
-      presentation[1].classList.remove("active");
-      presentation[3].classList.remove("active");
-      presentation[2].classList.add("active");
-
-      presentationTitle[1].classList.remove("active");
-      presentationTitle[0].classList.add("active");
-}
-
-presentationItem[3].onclick = function() {
-    (this).classList.add("active");
-    presentationItem[0].classList.remove("active");
-    presentationItem[1].classList.remove("active");
-    presentationItem[2].classList.remove("active");
-
-    presentation[0].classList.remove("active");
-    presentation[1].classList.remove("active");
-    presentation[2].classList.remove("active");
-    presentation[3].classList.add("active");
-
-    presentationTitle[0].classList.remove("active");
-    presentationTitle[1].classList.add("active");
-}
+        const settingTitle = [
+            {
+                idxChangeTitle: 0,
+                title: `Reliable and trustworthy by design: <span>readable by your devices and your own eyes.</span>`
+            },
+            {
+                idxChangeTitle: 1,
+                title: `Reliable and trustworthy by design: <span>readable by your devices and your own eyes.</span>`
+            },
+            {
+                idxChangeTitle: 2,
+                title: `Reliable and trustworthy by design: <span>readable by your devices and your own eyes.</span>`
+            },
+            {
+                idxChangeTitle: 3,
+                title: `How it works`
+            }
+        ];
 
 
-// document.querySelectorAll('.answers__item-question').forEach((item) =>
-//     item.addEventListener('click', () => {
-//         item.parentNode.classList.toggle('active');
-//     })
-// )
+        function chooseBlockImageAndAcordeons(textlist, imagelist, title, settingTitle, activeElement) {
+
+            textlist.forEach((item, idx) => {
+
+                item.addEventListener('click', () => {
+
+                    textlist.forEach((other, idx) => {
+                        if (other !== item) {
+                            other.classList.remove("active");
+                            imagelist[idx].classList.remove("active");
+                            return
+                        }
+                        return;
+                    });
+
+                    imagelist[idx].classList.add("active");
+                    title.innerHTML = settingTitle[idx].title;
+                    item.classList.add("active");
+                });
+            });
+
+            imagelist[activeElement].classList.add("active");
+            title.innerHTML = settingTitle[activeElement].title;
+            textlist[activeElement].classList.add("active");
+
+        };
+
+        chooseBlockImageAndAcordeons(presentationItem, presentation, presentationTitle, settingTitle,3);
+
+  
+
+            $('.quotes__slider-content').slick({
+                dots: true,
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                prevArrow: '<button type="button" class="slick-btn slick-prev">Previous</button>',
+                nextArrow: '<button type="button" class="slick-btn slick-next">Previous</button>'
+            });
+
+     
+
+        //https://github.com/ganlanyuan/tiny-slider
+        // http://ganlanyuan.github.io/tiny-slider/demo/
+        // var slider = tns({
+        //     items: 2,
+        //     container: "#slider_home",
+        //     speed: 400
+        // });
+
+
+    
+    };
+
+
+
+
+    if (document.querySelector('#faq')) {
+        console.log('fack')
+        document.querySelectorAll('.answers__category-title').forEach((item) =>
+            item.addEventListener('click', () => item.parentNode.classList.toggle('active'))
+        )
+
+        document.querySelectorAll('.answers__item-question').forEach((item) =>
+            item.addEventListener('click', () => item.parentNode.classList.toggle('active'))
+        )
+    }
+
+
+    if (document.querySelector('#security')) {
+
+    }
+
+
+
+    $(".menu__list-item--anchor").on("click", "a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id = $(this).attr('href'),
+
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
+
+
+
+});
+
+
+console.log('global')
+
+
